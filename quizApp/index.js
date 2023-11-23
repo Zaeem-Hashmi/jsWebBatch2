c03fb0582755b7bc76e41860de43148013982d3d
Zaeem-Hashmi <syedzae123@gmail.com>
2023-11-23 16:35:37 +0500

    quiz app part 1

--- quizApp/index.js
+++ quizApp/index.js
@@ -1,11 +1,36 @@
-class Data {
-    constructor(url){
-        this.getData(url)
-    }
-    async getData(url){
-        this.data1 = await fetch(url);
-        this.data_json = await this.data1.json();
-        console.log(this.data_json);
+let data =[];
+let question_number = 0;
+getdata();
+async function getdata() {
+    data = JSON.parse(sessionStorage.getItem('data'));
+}
+
+function printQuestion() {
+    question_data = data[question_number];
+    document.getElementById('last_number').innerText = data.length;
+    document.getElementById('first_number').innerText = question_number+1;
+    document.getElementById('question').innerText = question_data.question;
+    document.querySelector('#ans').innerHTML = "";
+    question_data.options.forEach(element => {
+        document.querySelector('#ans').innerHTML += `<div class="my-3 border border-primary p-2" style="background-color: #EFFAFF;">${element}</div>`
+    });
+}
+printQuestion();
+
+function questionSwap(type) {
+    if (type == "next") {
+        if (question_number < data.length-1) {
+            question_number++;
+            printQuestion()
+        }
+    }else if(type == "previous"){
+        if (question_number > 0) {
+            question_number--;
+            printQuestion();
+        }
     }
 }
-const instanceData = new Data('./data/question.json');
+
+
+
+
--- quizApp/js/data.js
+++ quizApp/js/data.js
@@ -1,15 +1,14 @@
-class data {
+class Data {
     constructor(url){
         this.getData(url)
     }
-    getData(url){
-        fetch(url).then((el)=>{
-            data = el.json();
-            data.then((value)=>{
-                sessionStorage.setItem("data",JSON.stringify(data));
-            })
-        }).catch((e)=>{
-            console.log(e);
-        })
+    async getData(url){
+        this.data1 = await fetch(url);
+        this.data_json = await this.data1.json();
+        this.storeDataInSessonStorage();
+    }
+    storeDataInSessonStorage() {
+        sessionStorage.setItem('data',JSON.stringify(this.data_json));
     }
 }
+const instanceData = new Data('./data/question.json');
