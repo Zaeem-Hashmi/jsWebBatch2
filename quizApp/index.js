d5715b56757d49b46ae1cfb8fb22d3c5533b37e2
Zaeem-Hashmi <syedzae123@gmail.com>
2023-11-24 16:37:10 +0500

    quiz app part2 completed

--- quizApp/index.js
+++ quizApp/index.js
@@ -1,5 +1,6 @@
-let data =[];
+let data = [];
 let question_number = 0;
+let score = 0;
 getdata();
 async function getdata() {
     data = JSON.parse(sessionStorage.getItem('data'));
@@ -8,25 +9,45 @@ async function getdata() {
 function printQuestion() {
     question_data = data[question_number];
     document.getElementById('last_number').innerText = data.length;
-    document.getElementById('first_number').innerText = question_number+1;
+    document.getElementById('first_number').innerText = question_number + 1;
     document.getElementById('question').innerText = question_data.question;
     document.querySelector('#ans').innerHTML = "";
-    question_data.options.forEach(element => {
-        document.querySelector('#ans').innerHTML += `<div class="my-3 border border-primary p-2" style="background-color: #EFFAFF;">${element}</div>`
+    question_data.options.forEach((element, key) => {
+        document.querySelector('#ans').innerHTML += `<div class="my-3 border border-primary p-2" style="background-color: #EFFAFF;cursor:pointer" onclick="check(${key})" id="option${key}">${element}</div>`
     });
 }
 printQuestion();
 
 function questionSwap(type) {
     if (type == "next") {
-        if (question_number < data.length-1) {
+        if (question_number < data.length - 1) {
             question_number++;
             printQuestion()
         }
-    }else if(type == "previous"){
+    } else if (type == "previous") {
         if (question_number > 0) {
             question_number--;
             printQuestion();
+        }
+    }
+}
+function check(key) {
+    if (data[question_number].correct_ans == key) {
+        score++;
+        document.getElementById(`option${key}`).classList.add('bg-success');
+        disableOption(key,data[question_number].correct_ans)
+    } else if (data[question_number].correct_ans != key) {
+        document.getElementById(`option${key}`).classList.add('bg-danger');
+        document.getElementById(`option${data[question_number].correct_ans}`).classList.add('bg-success');
+        disableOption(key,data[question_number].correct_ans)
+    }
+}
+function disableOption(key_pressed,rignt_ans) {
+    let options = document.querySelectorAll('#ans div');
+    for (var i = 0; i < Array.from(options).length; i++){
+        options[i].removeAttribute('onclick');
+        if (i != key_pressed) {
+            options[i].classList.add('disable');
         }
     }
 }
